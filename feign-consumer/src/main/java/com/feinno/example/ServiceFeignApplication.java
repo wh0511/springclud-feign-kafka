cb5adbc271075995fe2e0d2472a2b0d7149640ad
Li Yang <liyangsj@feinno.com>
2018-05-10 10:00:12 +0800

    enable hystrix

--- feign-consumer/src/main/java/com/feinno/example/ServiceFeignApplication.java
+++ feign-consumer/src/main/java/com/feinno/example/ServiceFeignApplication.java
@@ -9,15 +9,12 @@
  */
 package com.feinno.example;
 
-import feign.Feign;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
+
 
 /**
  * @author liyang
